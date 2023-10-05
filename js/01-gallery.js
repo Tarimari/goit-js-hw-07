12e712b1d1eee9e51e0b0678a6f3a2138b136556
Tarimari <tarashorban1987@gmail.com>
2023-10-05 14:35:21 +0300

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,13 +3,38 @@ import { galleryItems } from './gallery-items.js';
 
 const gallery = document.querySelector(`.gallery`);
 gallery.insertAdjacentHTML('afterbegin', makeGallery(galleryItems));
+gallery.addEventListener(`click`, handlerClick);
+
 
 function makeGallery(gallery) {
-    return gallery.map(({preview, description}) =>
-        `<li class="gallery__link">
-        <img class="gallery__image" src="${preview}" alt="${description}" />
-      </li>`)
+    return gallery.map(({preview, original, description}) =>
+        `<li class="gallery__item">
+  <a class="gallery__link" href='${original}'>
+    <img
+      class="gallery__image"
+      src='${preview}'
+      data-source='${original}'
+      alt='${description}'
+    />
+  </a>
+</li>`)
         .join(""); 
 }
 
-console.log(galleryItems);
+function handlerClick(evt) {
+  evt.preventDefault();
+  console.dir(evt.target);
+  console.log(evt.currentTarget === evt.target);
+  if (evt.currentTarget === evt.target) { return; }
+  
+  const instance = basicLightbox.create(`
+  <img class="gallery__image"
+  src='${evt.target.dataset.source}'
+  alt='${evt.target.alt}' 
+  />
+`);
+instance.show();
+
+}
+
+console.log(basicLightbox);
