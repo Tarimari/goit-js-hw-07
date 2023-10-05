75ecd1f17e09a8877a5932ac311f2a1e32e89e74
Tarimari <tarashorban1987@gmail.com>
2023-10-06 01:30:13 +0300

    0

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,17 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+let gallery = new SimpleLightbox('.gallery a');
+gallery.on('show.simplelightbox', function () {
+	// do something…
+});
 
+gallery.on('error.simplelightbox', function (e) {
+	console.log(e); // some usefull information
+});
+
+// with jQuery nearly the same
+let gallery = $('.gallery a').simpleLightbox();
+gallery.on('show.simplelightbox', function () {
+	// do something…
+});
 console.log(galleryItems);
